e91f57e788a231d95e968568928649b3de1e4ec7
Shrike6 <cody.stubbs@gmail.com>
2022-07-04 12:54:00 -0500

    Added functionality for save button

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,3 +2,31 @@
 // Set past, present, future colors to time
 // Save user inputs to local storage
 // 
+
+var currentTime = document.getElementById('currentDay');
+var saveButton9 = document.querySelector('.saveBtn9');
+var textArea = document.querySelector('.description');
+var textInput9 = document.getElementById('text9');
+
+currentTime.innerText = moment().format("dddd, MMMM Do YYYY");
+
+//Function to apply color to text area based on current time 
+// function getTime () {
+
+// }
+
+// checkTime every 5 minutes
+// setInterval(getTime(), (1000 * 60) * 5);
+// console.log(getTime);
+
+function saveEvent () {
+    this.event.target.parentElement.querySelector('input').value;
+    console.log(this.event.target.parentElement.querySelector('input').value);
+    localStorage.removeItem('textInput9');
+    localStorage.setItem('textInput9', textInput9.value);
+    console.log(localStorage);
+}
+
+textInput9.innerText = localStorage.getItem('textInput9');
+
+
